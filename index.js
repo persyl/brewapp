557813be6d9a9102b744ea64de98457124727715
Per Lundkvist <per.lundkvist@di.se>
2017-01-08 11:33:35 +0100

    Cleaned cloned repo files to start on new project

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//Old EcmaScript 5 syntax:
-//var HelloWorld = require('./src/hello-world.jsx');
+import BrewApp from './src/brewapp.jsx'; 
 
-import HelloPer from './src/hello-per.jsx'; //ES2015 syntax
-
-
-
-// ReactDOM.render(
-//   <HelloWorld country="Sverige" />,
-//   document.getElementById('example-output')
-// );
 
 ReactDOM.render(
-  <HelloPer />,
-  document.getElementById('example-output')
+  <BrewApp />,
+  document.getElementById('brewapp')
 );
---
+++ src/brewapp.jsx
@@ -0,0 +1,12 @@
+//EcmaScript 2015 syntax
+import React from'react';
+
+export default class BrewApp extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
+  render(){
+    return (<div><p>Välkommen till Brewapp!</p></div>);
+  }
+}
